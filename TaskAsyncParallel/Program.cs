2d9427f9d7d2a47f3f74f2f09b9de3ba2101e91e
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-29 16:23:21 +0200

    Add project files.

---
+++ TaskAsyncParallel/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Net.Sockets;
+
+string[] urls =
+{
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/comments",
+    "https://jsonplaceholder.typicode.com/albums"
+};
+
+
+Task<string>[] tasks = new Task<string>[urls.Length];
+
+for (int i = 0; i < urls.Length; i++)
+{
+    tasks[i] = FetchDataAsync(urls[i]);
+}
+
+string[] results = await Task.WhenAll(tasks);
+
+foreach (var result in results)
+    Console.WriteLine(result.Substring(0, 100));
+
+static async Task<string> FetchDataAsync(string url)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
